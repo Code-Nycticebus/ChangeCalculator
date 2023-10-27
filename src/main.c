86f75fce553e7e6cc87a90667273e097ac387b15
Loris Kriyonas <loris.kriyonas@gmail.com>
2023-10-27 16:10:45 +0200

    added bills and formatted better

--- src/main.c
+++ src/main.c
@@ -10,24 +10,36 @@ typedef enum CoinType {
   FRANKEN_1 = 100,
   FRANKEN_2 = 200,
   FRANKEN_5 = 500,
+  FRANKEN_10 = 1000,
+  FRANKEN_20 = 2000,
+  FRANKEN_50 = 5000,
+  FRANKEN_100 = 10000,
 } CoinType;
 
 const char *coin_type_to_str(CoinType coin) {
   switch (coin) {
   case RAPPEN_5:
-    return "5 Rp.";
+    return "  5 Rp.";
   case RAPPEN_10:
-    return "10 Rp.";
+    return " 10 Rp.";
   case RAPPEN_20:
-    return "20 Rp.";
+    return " 20 Rp.";
   case RAPPEN_50:
-    return "50 Rp.";
+    return " 50 Rp.";
   case FRANKEN_1:
-    return "1 Fr.";
+    return "  1 Fr.";
   case FRANKEN_2:
-    return "2 Fr.";
+    return "  2 Fr.";
   case FRANKEN_5:
-    return "5 Fr.";
+    return "  5 Fr.";
+  case FRANKEN_10:
+    return " 10 Fr.";
+  case FRANKEN_20:
+    return " 20 Fr.";
+  case FRANKEN_50:
+    return " 50 Fr.";
+  case FRANKEN_100:
+    return "100 Fr.";
   }
 }
 
@@ -45,14 +57,14 @@ DivisionResult division(uint32_t a, uint32_t b) {
 
 void find_change(uint32_t change) {
   CoinType coins[] = {
-      FRANKEN_5, FRANKEN_2, FRANKEN_1, RAPPEN_50,
-      RAPPEN_20, RAPPEN_10, RAPPEN_5,
+      FRANKEN_100, FRANKEN_50, FRANKEN_20, FRANKEN_10, FRANKEN_5, FRANKEN_2,
+      FRANKEN_1,   RAPPEN_50,  RAPPEN_20,  RAPPEN_10,  RAPPEN_5,
   };
   const int coins_count = sizeof(coins) / sizeof(coins[0]);
   for (int i = 0; i < coins_count; i++) {
     DivisionResult res = division(change, coins[i]);
     if (0 < res.divisor) {
-      printf("%s:\t%d\n", coin_type_to_str(coins[i]), res.divisor);
+      printf("% 2d x %s\n", res.divisor, coin_type_to_str(coins[i]));
       change -= coins[i] * res.divisor;
     }
     if (res.remainder == 0) {
